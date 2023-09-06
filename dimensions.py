b9c7c8b970653ae71955307762b5f382658bf3e9
Max <38260677+maximilian-heeg@users.noreply.github.com>
2023-09-06 14:39:12 -0700

    Simplyfy container

--- dimensions.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env/ python
-import tifffile
-
-tif = tifffile.TiffFile('data/morphology_mip.ome.tif')
-page = tif.pages[0]  # get shape and dtype of image in first page
-pixel = page.shape[0] * page.shape[1]
-bytes = pixel * 16
-tif.close()
